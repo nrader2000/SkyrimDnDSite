d69d5540753bca46f0d23ac5888db0a401084b65
Nick Rader <69931749+nrader2000@users.noreply.github.com>
2021-04-22 12:14:01 -0400

    Update Database.php

--- Database.php
+++ Database.php
@@ -1,2 +1,44 @@
 <?php
     require_once("Resources/database_access.php");
+
+    //Get data from weapons table
+    $query = "SELECT * FROM Weapons";
+?>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>SkyrimDnD</title>
+	        <link rel="site icon"  type="image/png"  href="Images/SkyrimDnD-Symbol.png">
+            <!-- Inclusion of the css style sheet -->
+            <link rel="stylesheet" href="Resources/SiteStyles.css"> 
+        </head>
+        <body>
+            <!-- Use of the wrapper class around the whole body content -->
+            <div id="wrapper">
+            <!-- Title for homepage -->
+            <div class="header">
+                <h1><strong>Database</strong></h1>
+            </div>
+            <hr>
+            <!-- Navagation Section -->
+            <div class="topnav">
+                <div class="topnav-centered">
+                    <a href="index.html">Home</a>
+                    <a href="About.html">About</a>
+                    <a href="Database.php">Database</a>
+                    <a href="Resources.html">Resources</a>
+                    <a href="News.html">News</a>
+                    <a href="Connect.html">Connect</a>
+                </div>
+            </div>
+            <hr>
+            <!-- Main Content of Page -->
+            
+            <hr>
+            <!-- Footer w/ Copyright -->
+            <div class="footer">
+                <hr><p>Copyright &copy; 2021 Nick Rader</p>
+            </div>
+            </div>
+        </body>
+    </html>
